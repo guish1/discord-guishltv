d01bd1f7fd3dac2fa16af887260de14612bc39e3
Maxime LEBON <lemixam@hotmail.com>
2019-09-10 09:37:53 +0200

    Fix setAuthor

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var createEmbed = (title) => {
     var embed = new Discord.MessageEmbed()
     .setColor(0x00AE86)
     .setTitle(title)
-    .setAuthor("GUISHLTV" , "https://i.imgur.com/G34L4R7.png", "www.avisdetemplate.fr")
+    .setAuthor("GUISHLTV", "https://i.imgur.com/G34L4R7.png", "https://www.avisdetemplate.fr")
     .setFooter("© Powered by GUISH 2019 - Unofficial HLTV Bot")
     .setTimestamp();
     return embed;
