56934c5388b939e9f4ae62b576beb94f74793a6b
Maxime LEBON <lemixam@hotmail.com>
2019-09-11 21:42:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,8 +51,7 @@ client.on("message", message => {
             case "ranking":
             (async function () {
                 if (args[0] == "team") {
-                    //const res = await HLTV.getTeamRanking();
-                    const res = await Promise.all(res.map(res => HLTV.getTeamRanking()));
+                    const res = await HLTV.getTeamRanking();
                     var embed = createEmbed("Top 21 Team ranking");
                     for(var i = 0; i < 21; i++) {
                         // get team informations
