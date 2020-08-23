2e892b1232b5c1be3ad55b87cd66b411d66056d3
Maxime LEBON <lemixam@hotmail.com>
2020-08-24 01:28:03 +0200

    test : add URL to team name with cache

--- src/services/messages/format.js
+++ src/services/messages/format.js
@@ -64,7 +64,6 @@ const formatTeam = ({
 }) => {
   const slug = formatSlug(name);
   const url = `https://www.hltv.org/team/${id}/${encodeURIComponent(slug)}`;
-  // remove ${url} from link
   return {
     id,
     name,
@@ -74,7 +73,7 @@ const formatTeam = ({
     url,
     rank,
     flag: getCountryFlagEmoji(location),
-    link: `[${name}]( 'id: ${id}')`,
+    link: `[${name}](${url} 'id: ${id}')`,
   };
 };
 
