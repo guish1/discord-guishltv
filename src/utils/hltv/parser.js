3d94ecaef62d66698931fb4c1da19cd28478603d
Maxime LEBON <lemixam@hotmail.com>
2020-08-27 10:39:10 +0200

    feat: new results parser

--- src/utils/hltv/parser.js
+++ src/utils/hltv/parser.js
@@ -5,20 +5,34 @@
  * @returns {object} Team results if valid format.
  */
 const parseMapResult = (mapResult) => {
-  const parsed = mapResult.match(/^(\d+:\d+)/i);
+  /*const parsed = mapResult.match(/^(\d+:\d+)/i);
 
   if (!parsed) {
     return { isValid: false };
   }
 
   const [, score] = parsed;
+  */
+  const r1 = mapResult.substring(0, mapResult.indexOf('\n'));
+  r1 = r1.replace(";", "");
+  r1 = r1.split(' ');
+
+  const score1 = 0;
+  const score2 = 0;
+  for (var i = 0; i < r1.length; i++) {
+    var score = r1[i].split(":");
+    score1 = score1 + parseInt(score[0].replace("(", ""));
+    score2 = score2 + parseInt(score[1].replace(")", ""));
+  }
 
   const [team1Score, team2Score] = score.split(':');
 
   return {
     isValid: true,
-    team1: +team1Score,
-    team2: +team2Score,
+    //team1: +team1Score,
+    //team2: +team2Score,
+    team1: score1,
+    team2: score2,
   };
 };
 
